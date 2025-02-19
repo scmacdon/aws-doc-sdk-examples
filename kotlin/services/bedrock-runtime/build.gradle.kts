5063621c26cfcad24e955223f97191b5a973720f
scmacdon <scmacdon@amazon.com>
2025-02-19 13:50:57 -0500

    updated kotlin

---
+++ kotlin/services/bedrock-runtime/build.gradle.kts
@@ -0,0 +1,56 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.9.0"
+    kotlin("plugin.serialization") version "1.5.31"
+    application
+}
+
+group = "me.scmacdon"
+version = "1.0-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+buildscript {
+    repositories {
+        maven("https://plugins.gradle.org/m2/")
+    }
+
+    dependencies {
+        classpath("org.jlleitschuh.gradle:ktlint-gradle:10.3.0")
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+apply(plugin = "org.jlleitschuh.gradle.ktlint")
+
+dependencies {
+    implementation(platform("aws.sdk.kotlin:bom:1.3.112"))
+    implementation("aws.sdk.kotlin:bedrockruntime")
+    implementation("aws.sdk.kotlin:secretsmanager")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.2")
+    implementation("aws.smithy.kotlin:http-client-engine-okhttp")
+    implementation("aws.smithy.kotlin:http-client-engine-crt")
+    implementation("com.google.code.gson:gson:2.10")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+}
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "17"
+}
+
+tasks.test {
+    useJUnitPlatform() // Use JUnit 5 for running tests
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
+
+    // Define the test source set
+    testClassesDirs += files("build/classes/kotlin/test")
+    classpath += files("build/classes/kotlin/main", "build/resources/main")
+}
